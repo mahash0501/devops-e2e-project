f1902bf7074ab587937425254fdeb39fd4670ef5
mahash0501 <mahash0501@gmail.com>
2026-08-20 05:37:59 +0000

    feat: initial python service, unit tests, and Dockerfile

---
+++ app/test_main.py
@@ -0,0 +1,7 @@
+from main import app
+
+def test_health_check():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json["status"] == "healthy"
